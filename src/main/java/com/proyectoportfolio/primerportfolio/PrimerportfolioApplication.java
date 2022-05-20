dd73ed223ca57a3d14a90fefa93b9acbd343694c
Vanesa Curci <curcivanesa@gmail.com>
2022-05-19 23:10:02 -0300

    cors

--- src/main/java/com/proyectoportfolio/primerportfolio/PrimerportfolioApplication.java
+++ src/main/java/com/proyectoportfolio/primerportfolio/PrimerportfolioApplication.java
@@ -2,6 +2,9 @@ package com.proyectoportfolio.primerportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PrimerportfolioApplication {
@@ -9,5 +12,16 @@ public class PrimerportfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrimerportfolioApplication.class, args);
 	}
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+	       return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://portafolio-web-70535.web.app/");
+			}
+		};
+	}
 
 }
+
